4f1c26d78a161141bf093735032a26c0bbbd0d11
Antonio Renteria <antrent@gmail.com>
2024-02-25 00:38:58 -0500

    creacion metodo get y manejo de estados de respuestas

--- MagicVilla_API/Controllers/VillaController.cs
+++ MagicVilla_API/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MagicVilla_API.Datos;
+using MagicVilla_API.Modelos.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_API.Controllers
@@ -7,5 +8,30 @@ namespace MagicVilla_API.Controllers
     [ApiController]
     public class VillaController : ControllerBase
     {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<VillaDto>> GetVillas() 
+        {
+            return Ok(VillaStore.villaList);
+        }
+
+        [HttpGet("id:int")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<VillaDto> GetVilla(int id)
+        {
+            if (id==0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(v => v.Id == id);
+
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            return Ok(villa);
+        }
     }
 }
--- MagicVilla_API/Datos/VillaStore.cs
+++ MagicVilla_API/Datos/VillaStore.cs
@@ -1,7 +1,13 @@
-﻿namespace MagicVilla_API.Datos
+﻿using MagicVilla_API.Modelos.Dto;
+
+namespace MagicVilla_API.Datos
 {
     public static class VillaStore
     {
-      public static IDataTokensMetadata;
+        public static List<VillaDto> villaList = new List<VillaDto>
+         {
+             new VillaDto {Id=1, Name= "Vista a la piscina"},
+             new VillaDto {Id=2, Name= "Vista a la Playa"}
+         };
     }
 }
