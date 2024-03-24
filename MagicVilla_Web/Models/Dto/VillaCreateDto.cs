a815cee5f2fc4569d70803ec85ae7f41dd29ea35
Antonio Renteria <antrent@gmail.com>
2024-03-24 16:45:25 -0500

    creacion modelo web para consultar Villa

---
+++ MagicVilla_Web/Models/Dto/VillaCreateDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_Web.Models.Dto
+{
+    public class VillaCreateDto
+    {
+        [Required]
+        [MaxLength(30)]
+        public string Nombre { get; set; }
+        public string Detalle { get; set; }
+
+        [Required]
+        public double Tarifa { get; set; }
+        public int Ocupantes { get; set; }
+        public int MetrosCuadrados { get; set; }
+        public String ImagenUrl { get; set; }
+        public String Amenidad { get; set; }
+    }
+}
